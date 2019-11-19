350b173469e3c6c55c30d572ba0a5edb5e550369
Guillaume NISI <g.nisi@student.isartdigital.com>
2019-11-19 16:52:45 +0100

    oui

--- include/Mat4.hpp
+++ include/Mat4.hpp
@@ -15,9 +15,15 @@ public:
     Mat4(Vec4 (&array)[4]);
     Mat4(float (&array)[16]);
     ~Mat4();
+    
+    static Mat4 CreateTranslationMatrix(const Vec3 &translation);
+    static Mat4 CreateScaleMatrix(const Vec3 &scale);
+    static Mat4 CreateXRotationMatrix(float angle);
+    static Mat4 CreateYRotationMatrix(float angle);
+    static Mat4 CreateZRotationMatrix(float angle);
 
     static Mat4 CreateTransformMatrix(const Vec3 &rotation, const Vec3 &position, const Vec3 &scale);
 
     Mat4 operator*(Mat4 &other);
-    Vec4 operator*(float f);
+    Vec4 Mat4::operator*(Vec4 &v);
 };
--- src/Mat4.cpp
+++ src/Mat4.cpp
@@ -1,7 +1,7 @@
 #include "Mat4.hpp"
 
-Mat4::Mat4() : mat{0}{}
-Mat4::Mat4(float f): mat{f}{}
+Mat4::Mat4() : mat{0} {}
+Mat4::Mat4(float f) : mat{f} {}
 
 Mat4::Mat4(Vec4 (&array)[4])
 {
@@ -11,7 +11,41 @@ Mat4::Mat4(float (&array)[16])
 {
 }
 
-Mat4::~Mat4(){}
+Mat4::~Mat4() {}
+
+static Mat4 CreateTranslationMatrix(const Vec3 &translation)
+{
+
+}
+
+static Mat4 CreateScaleMatrix(const Vec3 &scale)
+{
+
+}
+
+//AVX
+static Mat4 CreateXRotationMatrix(float angle)
+{
+    
+}
+
+//AVX
+static Mat4 CreateYRotationMatrix(float angle)
+{
+
+}
+
+//AVX
+static Mat4 CreateZRotationMatrix(float angle)
+{
+
+}
+
+//AVX
+static Mat4 CreateTransformMatrix(const Vec3 &rotation, const Vec3 &position, const Vec3 &scale)
+{
+
+}
 
 Mat4 Mat4::operator*(Mat4 &other)
 {
@@ -24,6 +58,6 @@ Mat4 Mat4::operator*(Mat4 &other)
     return res;
 }
 
-Vec4 Mat4::operator*(float f)
+Vec4 Mat4::operator*(Vec4 &v)
 {
 }
