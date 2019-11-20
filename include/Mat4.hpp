abed1a6893a846080717fa18586b564dafaf78e8
Guillaume NISI <g.nisi@student.isartdigital.com>
2019-11-20 14:47:56 +0100

    transform

--- include/Mat4.hpp
+++ include/Mat4.hpp
@@ -1,32 +1,30 @@
 #pragma once
 #include "Vec4.hpp"
 
-
 // Column major library
 class Mat4
 {
 public:
     union {
-        Vec4 v_mat[4];
-        float mat[16];
-        float _mat[4][4];
+        Vec4  v[4];
+        float a[16];
+        float aa[4][4];
     };
 
     Mat4();
     Mat4(float f);
-    // Mat4(const Vec4 (&array)[4]);
     Mat4(const float (&array)[16]);
-    Mat4(const Vec4& v1,const Vec4& v2,const Vec4& v3,const Vec4& v4);
+    Mat4(const Vec4 &v1, const Vec4 &v2, const Vec4 &v3, const Vec4 &v4);
     ~Mat4();
-    
+
     static Mat4 CreateTranslationMatrix(const Vec3 &translation);
     static Mat4 CreateScaleMatrix(const Vec3 &scale);
-    static Mat4 CreateXRotationMatrix(float angle);
-    static Mat4 CreateYRotationMatrix(float angle);
-    static Mat4 CreateZRotationMatrix(float angle);
+    static Mat4 CreateXRotationMatrix(const float angle);
+    static Mat4 CreateYRotationMatrix(const float angle);
+    static Mat4 CreateZRotationMatrix(const float angle);
+    static Mat4 CreateTransformMatrix(const Vec3 &position, const Vec3 &rotation, const Vec3 &scale);
 
-    static Mat4 CreateTransformMatrix(const Vec3 &rotation, const Vec3 &position, const Vec3 &scale);
-
-    Mat4 operator*(Mat4 &other);
-    Vec4 Mat4::operator*(Vec4 &v);
+    Mat4 operator+(const Mat4 &other);
+    Mat4 operator*(const Mat4 &other);
+    Vec4 operator*(const Vec4 &v);
 };
