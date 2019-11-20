6349c153094b368b0c3375e7324735a6e1da8136
Guillaume NISI <g.nisi@student.isartdigital.com>
2019-11-20 13:27:40 +0100

    modifs vect 4

--- src/Mat4.cpp
+++ src/Mat4.cpp
@@ -3,23 +3,25 @@
 
 Mat4::Mat4() : mat{0} {}
 Mat4::Mat4(float f) : mat{f} {}
-
-Mat4::Mat4(const Vec4  (&array)[4]) : v_mat{*array}
+// Mat4::Mat4(const Vec4 (&array)[4]) : v_mat{*array} {}
+Mat4::Mat4(const Vec4 &v1, const Vec4 &v2, const Vec4 &v3, const Vec4 &v4)
 {
+    v_mat[0] = v1;
+    v_mat[1] = v2;
+    v_mat[2] = v3;
+    v_mat[3] = v4;
 }
 
 Mat4::Mat4(const float (&array)[16]) : mat{*array} {}
-
 Mat4::~Mat4() {}
 
 static Mat4 CreateTranslationMatrix(const Vec3 &translation)
 {
-
 }
 
 static Mat4 CreateScaleMatrix(const Vec3 &scale)
 {
-    return Mat4{{scale.x,0,0,0,scale.y,0,0,0,scale.z}};
+    return Mat4{{scale.x, 0, 0, 0}, {0, scale.y, 0, 0}, {0, 0, scale.z, 0}, {0, 0, 0, 1}};
 }
 
 //AVX
@@ -31,7 +33,7 @@ static Mat4 CreateXRotationMatrix(float angle)
     float c = cosf(angle * M_PI / 180);
     float s = sinf(angle * M_PI / 180);
 
-    return Mat4{{1, 0, 0, 0, c, s, 0, -s, c}};
+    return Mat4{{1, 0, 0, 0}, {0, c, s, 0}, {0, -s, c, 0}, {0, 0, 0, 1}};
 }
 
 //AVX
@@ -55,7 +57,6 @@ static Mat4 CreateZRotationMatrix(float angle)
 //AVX
 static Mat4 CreateTransformMatrix(const Vec3 &rotation, const Vec3 &position, const Vec3 &scale)
 {
-    
 }
 
 Mat4 Mat4::operator*(Mat4 &other)
@@ -73,5 +74,5 @@ Vec4 Mat4::operator*(Vec4 &v)
 {
     for (size_t i = 0; i < 4; i++)
         for (size_t j = 0; j < 4; j++)
-            //
+    //
 }
