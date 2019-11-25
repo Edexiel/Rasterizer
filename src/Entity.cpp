f2a9e47b0b472246f3d4ea3d1cefed1c2983714a
Guillaume NISI <g.nisi@student.isartdigital.com>
2019-11-25 11:54:02 +0100

    ajout de fonctions simplifiant le travail sur matrices

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,7 +1,35 @@
 #include "Entity.hpp"
 
-Entity::Entity(): transfo{Mat4::identity()}{}
-Entity::Entity(Mesh* _mesh) : mesh{_mesh},transfo{Mat4::identity()}{}
-Entity::Entity(Mesh* _mesh, Mat4& _transfo) : mesh{_mesh},transfo{_transfo}{}
+Entity::Entity() : transfo{Mat4::identity()} {}
+Entity::Entity(Mesh *_mesh) : mesh{_mesh}, transfo{Mat4::identity()} {}
+Entity::Entity(Mesh *_mesh, Mat4 &_transfo) : mesh{_mesh}, transfo{_transfo} {}
 
-Entity::~Entity(){}
+Entity::~Entity() {}
+
+void Entity::scale(float x, float y, float z)
+{
+    transfo = transfo * Mat4::CreateScaleMatrix({x,y,z});
+}
+
+void Entity::translate(float x, float y, float z)
+{
+    transfo = transfo * Mat4::CreateTranslationMatrix({x,y,z});
+}
+
+/*
+ *  float x : angle in radian
+ *  float z : angle in radian
+ *  float y : angle in radian
+ */
+void Entity::rotate(float x, float y, float z)
+{
+    if(x != 0.f)
+        transfo = transfo * Mat4::CreateXRotationMatrix(x);
+
+    if(y != 0.f)
+        transfo = transfo * Mat4::CreateYRotationMatrix(y);
+
+    if(z != 0.f)
+        transfo = transfo * Mat4::CreateZRotationMatrix(z);
+
+}
