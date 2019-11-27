85570700793944ca71f4414265bd6d98f63015af
Alex FIGUEIREDO <a.figueiredo@student.isartdigital.com>
2019-11-27 10:47:17 +0100

    fix the color of the triangle

--- src/Rasterizer.cpp
+++ src/Rasterizer.cpp
@@ -107,14 +107,14 @@ void Rasterizer::upload_texture() const
 void Rasterizer::draw_triangle(Vertex v1, Vertex v2, Vertex v3, Mat4 &transformation)
 {
     // Mat4 ortho {{2/(aspect+aspect), 0, 0, 0}, {0, 1, 0, 0}, {0, 0, -1, 0}, {-((aspect-aspect)/(aspect+aspect)),-(0 /2),-(1 +- 1), 1}};
-    Mat4 mat_finale = viewport * projection * transformation;
+    Mat4 mat_finale =   projection * viewport * transformation;
 
     v1.position = (mat_finale * Vec4{v1.position, 1}).xyz;
     v2.position = (mat_finale * Vec4{v2.position, 1}).xyz;
     v3.position = (mat_finale * Vec4{v3.position, 1}).xyz;
 
-    Vec3 vec1{v1.position.x - v2.position.x, v1.position.y - v2.position.y, 0};
-    Vec3 vec2{v3.position.x - v2.position.x, v3.position.y - v2.position.y, 0};
+    Vec3 vec1{v2.position.x - v1.position.x, v2.position.y - v1.position.y, 0};
+    Vec3 vec2{v3.position.x - v1.position.x, v3.position.y - v1.position.y, 0};
 
     uint xMin = min(min(v1.position.x, v2.position.x), v3.position.x) - 1;
     uint xMax = max(max(v1.position.x, v2.position.x), v3.position.x) + 1;
@@ -136,7 +136,7 @@ void Rasterizer::draw_triangle(Vertex v1, Vertex v2, Vertex v3, Mat4 &transforma
     {
         for (uint x = xMin; x < xMax; x++)
         {
-            Vec3 q{x - v2.position.x, y - v2.position.y, 0};
+            Vec3 q{x - v1.position.x, y - v1.position.y, 0};
             //AVX
             float w1 = cross_product(q, vec2) / cross_product(vec1, vec2);
             float w2 = cross_product(vec1, q) / cross_product(vec1, vec2);
@@ -145,17 +145,18 @@ void Rasterizer::draw_triangle(Vertex v1, Vertex v2, Vertex v3, Mat4 &transforma
             if (w1 >= 0.f && w2 >= 0.f && w1 + w2 <= 1)
             {
                 float z = v1.position.z * w1 + v2.position.z * w2 + v3.position.z * w3;
-                set_pixel_color(x, y, z, {v1.color * w1 + v2.color * w2 + v3.color * w3});
+                set_pixel_color(x, y, z, {v1.color * w3 + v2.color * w1 + v3.color * w2});
             }
         }
     }
 }
 
-void Rasterizer::draw_point(Vertex v, Mat4 &transfo)
+void Rasterizer::draw_point(Vertex v, Mat4 &transformation)
 {
-    v.position = (transfo * Vec4{v.position, 1}).xyz;
+    Mat4 mat_finale = viewport * projection * transformation;
+    v.position = (mat_finale * Vec4{v.position, 1}).xyz;
     // viewport
-    set_pixel_color(v.position.x, v.position.y, 0, v.color);
+    set_pixel_color(v.position.x, v.position.y, v.position.z, v.color);
 }
 
 void Rasterizer::set_pixel_color(uint x, uint y, uint z, const Color &c)
