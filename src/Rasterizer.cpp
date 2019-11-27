adb5c52c095157d4a5cedb1dcd23c909f36a5b2f
Alex FIGUEIREDO <a.figueiredo@student.isartdigital.com>
2019-11-27 10:11:21 +0100

    fix triangle color

--- src/Rasterizer.cpp
+++ src/Rasterizer.cpp
@@ -107,15 +107,14 @@ void Rasterizer::upload_texture() const
 void Rasterizer::draw_triangle(Vertex v1, Vertex v2, Vertex v3, Mat4 &transformation)
 {
     // Mat4 ortho {{2/(aspect+aspect), 0, 0, 0}, {0, 1, 0, 0}, {0, 0, -1, 0}, {-((aspect-aspect)/(aspect+aspect)),-(0 /2),-(1 +- 1), 1}};
-
     Mat4 mat_finale = viewport * projection * transformation;
 
     v1.position = (mat_finale * Vec4{v1.position, 1}).xyz;
     v2.position = (mat_finale * Vec4{v2.position, 1}).xyz;
     v3.position = (mat_finale * Vec4{v3.position, 1}).xyz;
 
-    Vec3 vec1{v2.position.x - v1.position.x, v2.position.y - v1.position.y, 0};
-    Vec3 vec2{v3.position.x - v1.position.x, v3.position.y - v1.position.y, 0};
+    Vec3 vec1{v1.position.x - v2.position.x, v1.position.y - v2.position.y, 0};
+    Vec3 vec2{v3.position.x - v2.position.x, v3.position.y - v2.position.y, 0};
 
     uint xMin = min(min(v1.position.x, v2.position.x), v3.position.x) - 1;
     uint xMax = max(max(v1.position.x, v2.position.x), v3.position.x) + 1;
@@ -137,7 +136,7 @@ void Rasterizer::draw_triangle(Vertex v1, Vertex v2, Vertex v3, Mat4 &transforma
     {
         for (uint x = xMin; x < xMax; x++)
         {
-            Vec3 q{x - v1.position.x, y - v1.position.y, 0};
+            Vec3 q{x - v2.position.x, y - v2.position.y, 0};
             //AVX
             float w1 = cross_product(q, vec2) / cross_product(vec1, vec2);
             float w2 = cross_product(vec1, q) / cross_product(vec1, vec2);
@@ -163,7 +162,7 @@ void Rasterizer::set_pixel_color(uint x, uint y, uint z, const Color &c)
 {
     if (z < depth_buffer[x + y * m_width])
     {
+    }
         color_buffer[x + y * m_width] = c;
         depth_buffer[x + y * m_width] = z;
-    }
 }
